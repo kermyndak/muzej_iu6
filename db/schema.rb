5c4f2848f6ed12f9f24f8a144449918ef9a707c8
kermyndak <kermyndak@bk.ru>
2023-07-16 22:55:23 +0300

    Change error message

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_214513) do
     t.string "name"
     t.string "surname"
     t.string "middle_name"
+    t.string "role", default: "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
